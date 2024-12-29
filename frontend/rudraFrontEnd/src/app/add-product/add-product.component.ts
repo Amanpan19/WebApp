24683bd82c7707603d2b5652c3dc49cd665552cf
Aman <amanp.jun1906@gmail.com>
2024-12-29 11:44:57 +0530

    Made changes in Add product component.. added product info

--- frontend/rudraFrontEnd/src/app/add-product/add-product.component.ts
+++ frontend/rudraFrontEnd/src/app/add-product/add-product.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { FormArray, FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '../service/product.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from '../service/user.service';
+import { productInfo } from '../model/productInfo';
+
 
 @Component({
   selector: 'app-add-product',
@@ -16,13 +18,17 @@ export class AddProductComponent implements OnInit{
 
   userEmail:any;
   productForm:any;
+  productInfo!:productInfo;
   selectedImage: any = File;
+  clothTypes = ['Cotton', 'Linen', 'Silk', 'Polyester'];
 
   constructor(
         private fb:FormBuilder,
         private productService:ProductService,
         private _snackBar:MatSnackBar,
-        private userSer:UserService
+        private userSer:UserService,
+        private el: ElementRef,
+        private renderer: Renderer2
         ){}
 
   ngOnInit(): void {
@@ -39,10 +45,46 @@ export class AddProductComponent implements OnInit{
       quantity:["",Validators.required],
       productRating:["",Validators.required],
       description:["",Validators.required],
+      productDetails: this.fb.group({
+        clothType: ['', Validators.required],
+        availableSize: this.fb.array([], Validators.required), 
+        return14DayAvailability: [false, Validators.required],
+        colorsAvail: this.fb.array([], Validators.required),
+        fashionType: ['', Validators.required],
+      })
     })
   }
-
   
+
+  get availableSize(): FormArray {
+    return this.productForm.get('productDetails.availableSize') as FormArray;
+  }
+
+  get colorsAvail(): FormArray {
+    return this.productForm.get('productDetails.colorsAvail') as FormArray;
+  }
+
+  // Add a new color
+  addColor(color: string) {
+    this.colorsAvail.push(this.fb.control(color, Validators.required));
+  }
+
+  // Remove a color
+  removeColor(index: number) {
+    this.colorsAvail.removeAt(index);
+  }
+
+  onCheckboxChange(event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.availableSize.push(this.fb.control(checkbox.value));
+    } else {
+      const index = this.availableSize.controls.findIndex(control => control.value === checkbox.value);
+      if (index !== -1) {
+        this.availableSize.removeAt(index);
+      }
+    }
+  }
 
 
   getProductId(){
@@ -70,6 +112,13 @@ export class AddProductComponent implements OnInit{
           duration: 2000,
           panelClass: ['mat-toolbar', 'mat-primary']
         });
+
+        this.productForm.reset();
+        this.selectedImage = null;
+  
+        this.availableSize.clear();
+        this.colorsAvail.clear();
+
       },
       error:err=>{
         this._snackBar.open('Product is not added.....', 'Failure', {
